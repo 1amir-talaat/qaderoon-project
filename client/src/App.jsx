7bb52e5904356d5deccd123dca19fe8e4f634c7f
1amir-talaat <hsiwh2be872b@gmail.com>
2025-01-30 23:47:55 +0200

    update about section

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,33 +1,41 @@
-import Navbar from "./components/Navbar/Navbar";
-import Footer from "./components/Footer/Footer";
+import { useEffect, lazy, Suspense } from "react";
+import { Route, Routes, BrowserRouter as Router } from "react-router-dom";
 import "aos/dist/aos.css";
 import AOS from "aos";
-import { useEffect } from "react";
-import { Route, Routes, BrowserRouter as Router } from "react-router-dom";
-import Home from "./pages/Home";
-import AllArticles from "./pages/AllArticles";
-import ArticleDetails from "./pages/ArticleDetails.jsx";
-import AddNews from "./dashboard/AddNews";
-import Whowe from "./components/AboutUS/WhoWe/Whowe";
-import Speech from "./components/AboutUS/Speech/Speech";
-import Governorates from "./components/AboutUS/Governorates/Governorates";
-import SuccessPartners from "./components/AboutUS/SuccessPartners/SuccessPartners";
-import Units from "./components/AboutUS/Units/units.jsx";
-import Offices from "./components/AboutUS/Offices/Offices";
-import News from "./pages/News";
+
+// Import Static Components
+import Navbar from "./components/Navbar/Navbar";
+import Footer from "./components/Footer/Footer";
 import ScrollToTop from "./utils/ScrollToTop";
-import AddAuthor from "./dashboard/AddAuthor";
-import Board from "./components/AboutUS/BoardofTrustees/Board";
 import ComingSoon from "./utils/CoomingSoon/ComingSoon";
-import AdminLogin from "./dashboard/AdminLogin.jsx";
-import Dashboard from "./dashboard/Dashboard";
-import Supreme from "./components/AboutUS/Supreme/Supreme";
-import ContactUs from "./pages/ContactUs/ContactUs.jsx";
-import DashboardActions from "./dashboard/DashboardActions.jsx";
-import AddArticle from "./dashboard/AddArticle.jsx";
+
+// Lazy Loaded Components
+const Home = lazy(() => import("./pages/Home"));
+const AllArticles = lazy(() => import("./pages/AllArticles"));
+const ArticleDetails = lazy(() => import("./pages/ArticleDetails.jsx"));
+const News = lazy(() => import("./pages/News"));
+const ContactUs = lazy(() => import("./pages/ContactUs/ContactUs.jsx"));
+
+// Dashboard Lazy Loading
+const Dashboard = lazy(() => import("./dashboard/Dashboard"));
+const AdminLogin = lazy(() => import("./dashboard/AdminLogin.jsx"));
+const DashboardActions = lazy(() => import("./dashboard/DashboardActions.jsx"));
+const AddArticle = lazy(() => import("./dashboard/AddArticle.jsx"));
+const AddNews = lazy(() => import("./dashboard/AddNews"));
+const AddAuthor = lazy(() => import("./dashboard/AddAuthor"));
+
+// About Us Lazy Loading
+const Whowe = lazy(() => import("./components/AboutUS/WhoWe/Whowe"));
+const Speech = lazy(() => import("./components/AboutUS/Speech/Speech"));
+const Governorates = lazy(() => import("./components/AboutUS/Governorates/Governorates"));
+const SuccessPartners = lazy(() => import("./components/AboutUS/SuccessPartners/SuccessPartners"));
+const Units = lazy(() => import("./components/AboutUS/Units/units.jsx"));
+const Offices = lazy(() => import("./components/AboutUS/Offices/Offices"));
+const Board = lazy(() => import("./components/AboutUS/BoardofTrustees/Board"));
+const Supreme = lazy(() => import("./components/AboutUS/Supreme/Supreme"));
 
 function App() {
-  // initialize AOS
+  // Initialize AOS
   useEffect(() => {
     AOS.init({ duration: 1000, once: true });
   }, []);
@@ -36,42 +44,42 @@ function App() {
     <Router>
       {/* Ensures the page scrolls to the top on route change */}
       <ScrollToTop />
-
       <Navbar />
 
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/articles" element={<AllArticles />} />
-        <Route path="/article/:id" element={<ArticleDetails />} />
-        <Route path="/news" element={<News />} />
-        <Route path="contact" element={<ContactUs />} />
+      {/* Lazy Loading Components */}
+      <Suspense>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/articles" element={<AllArticles />} />
+          <Route path="/article/:id" element={<ArticleDetails />} />
+          <Route path="/news" element={<News />} />
+          <Route path="/contact" element={<ContactUs />} />
 
-        {/* Dashboard Routes */}
-        <Route path="dashboard">
-          {/* Login (public) */}
-          <Route path="login" element={<AdminLogin />} />
-            {/* Protected Routes (wrapped in Dashboard layout) */}
-          <Route element={<Dashboard />}>
-            <Route index element={<DashboardActions />} />
-            <Route path="add-article" element={<AddArticle />} />
-            <Route path="add-news" element={<AddNews />} />
-            <Route path="add-author" element={<AddAuthor />} />
+          {/* Dashboard Routes */}
+          <Route path="dashboard">
+            <Route path="login" element={<AdminLogin />} />
+            <Route element={<Dashboard />}>
+              <Route index element={<DashboardActions />} />
+              <Route path="add-article" element={<AddArticle />} />
+              <Route path="add-news" element={<AddNews />} />
+              <Route path="add-author" element={<AddAuthor />} />
+            </Route>
           </Route>
-        </Route>
 
-         {/* من نحن Section */}
-        <Route path="/about/institution" element={<Whowe />} />
-        <Route path="/about/speech" element={<Speech />} />
-        <Route path="/about/governorates" element={<Governorates />} />
-        <Route path="/about/success-partners" element={<SuccessPartners />} />
-        <Route path="/about/offices" element={<Offices />} />
-        <Route path="/about/board" element={<Board />} />
-        <Route path="/about/units" element={<Units />} />
-        <Route path="/about/supreme-advisory" element={<Supreme />} />
+          {/* About Us Section */}
+          <Route path="/about/institution" element={<Whowe />} />
+          <Route path="/about/speech" element={<Speech />} />
+          <Route path="/about/governorates" element={<Governorates />} />
+          <Route path="/about/success-partners" element={<SuccessPartners />} />
+          <Route path="/about/offices" element={<Offices />} />
+          <Route path="/about/board" element={<Board />} />
+          <Route path="/about/units" element={<Units />} />
+          <Route path="/about/supreme-advisory" element={<Supreme />} />
 
-        <Route path="*" element={<ComingSoon />} />
+          <Route path="*" element={<ComingSoon />} />
+        </Routes>
+      </Suspense>
 
-      </Routes>
       <Footer />
     </Router>
   );
