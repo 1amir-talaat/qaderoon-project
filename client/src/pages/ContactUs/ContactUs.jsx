cac7a7046d67866b39a79401b8dd70817987cd89
1amir-talaat <hsiwh2be872b@gmail.com>
2025-01-30 23:23:26 +0200

    add loader to contact us

--- client/src/pages/ContactUs/ContactUs.jsx
+++ client/src/pages/ContactUs/ContactUs.jsx
@@ -1,23 +1,35 @@
+import { useState } from "react";
 import "./contactUs.css";
 
 const ContactUs = () => {
+  const [loading, setLoading] = useState(true);
+
   return (
-    <>
-      <div className="container my-11 mx-auto">
-        <h2 className="font-extrabold text-3xl md:text-4xl pb-5 underlineR text-neutral-950 mb-6">
-          تواصل <span className="text-primary relative top-1">معنا</span>
-        </h2>
+    <div className="container my-11 mx-auto">
+      {/* Header is always visible */}
+      <h2 className="font-extrabold text-3xl md:text-4xl pb-5 underlineR text-neutral-950 mb-6">
+        تواصل <span className="text-primary relative top-1">معنا</span>
+      </h2>
+
+      {/* Iframe wrapper with loader */}
+      <div className="relative">
+        {loading && (
+          <div className="absolute inset-0 flex items-center justify-center bg-white bg-opacity-75 z-10 ">
+            <div className="loader "></div>
+          </div>
+        )}
         <iframe
           src={import.meta.env.VITE_Form_Facade_URL}
           width="100%"
-          height="1000px"
+          height={`${loading ? "88vh" : "1000px"}`}
           frameBorder="0"
           marginWidth="0"
-          marginHeight="0">
-          Loading…
-        </iframe>
+          marginHeight="0"
+          onLoad={() => setLoading(false)}
+          className="mt-14"
+        />
       </div>
-    </>
+    </div>
   );
 };
 
