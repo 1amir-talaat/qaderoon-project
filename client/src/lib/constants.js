6ca58fc894fb32952a1c8d89feb9ea2954c9f18a
AhmedOsman101 <trollingosman101@outlook.com>
2025-02-04 22:34:54 +0200

    feat(api): add axios api client & env config
    - Created axios instance for API calls
    - Added environment variable support
    - Configured base URL dynamically
    - Implemented Referer header for CORS
    - Updated .env.example file

--- client/src/lib/constants.js
+++ client/src/lib/constants.js
@@ -1,1 +1,8 @@
 export const ORG_NAME = "شباب قادرون";
+const ENVIRONMENT = import.meta.env.VITE_NODE_ENVIRONMENT;
+
+// Set base URL from environment variable or hardcode
+export const HOST_SERVER =
+  ENVIRONMENT == "production" ? "https://qyf-eg.org" : "http://localhost:5000";
+export const REFERER =
+  ENVIRONMENT == "production" ? "https://qyf-eg.org" : "http://localhost:5173";
