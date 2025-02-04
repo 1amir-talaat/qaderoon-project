27ef44ae01b619de21ff6591543011cef8db4de7
AhmedOsman101 <trollingosman101@outlook.com>
2025-02-04 20:25:38 +0200

    feat(client): update website title and add SEO
    - Added react-helmet for SEO
    - Updated page titles and meta descriptions
    - Improved website SEO optimization
    - Added Open Graph and Twitter Card tags
    - Updated package.json with react-helmet

--- client/src/pages/AllArticles.jsx
+++ client/src/pages/AllArticles.jsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react";
 import Card from "../utils/Card/Card";
 import { Link } from "react-router-dom";
 import Loader from "../utils/Loader/Loader";
+import { Helmet } from "react-helmet";
 import { isEmptyArray } from "../lib/utils";
 
 function AllArticles() {
@@ -16,7 +17,9 @@ function AllArticles() {
     const fetchData = async () => {
       try {
         setIsLoading(true);
-        const response = await axios.get(`${import.meta.env.VITE_HOST_SERVER}articles/`);
+        const response = await axios.get(
+          `${import.meta.env.VITE_HOST_SERVER}articles/`
+        );
 
         console.log("API Response Data:", response.data.data);
 
@@ -44,71 +47,117 @@ function AllArticles() {
     }
   };
 
-  const displayedArticles = articles.slice(currentPage * limit, (currentPage + 1) * limit);
+  const displayedArticles = articles.slice(
+    currentPage * limit,
+    (currentPage + 1) * limit
+  );
 
   return (
-    <div className="py-11 pb-16">
-      <h2 className="font-extrabold text-3xl md:text-4xl pb-5 underlineR text-neutral-950 mx-auto">
-        اخر <span className="text-primary relative top-1">المقالات</span>
-      </h2>
+    <>
+      <Helmet>
+        <title>قادرون | آخر المقالات</title>
+        <meta
+          name="description"
+          content="اكتشف أحدث المقالات على موقع قادرون. اطلع على مقالات متنوعة في مجالات التنمية الذاتية، الصحة النفسية، والتكنولوجيا. تصفح الآن للمزيد من المعرفة!"
+        />
+        <meta
+          name="keywords"
+          content="مقالات, تطوير الذات, الصحة النفسية, التكنولوجيا, آخر المقالات, قادرون, نصائح عملية, تثقيف صحي, شباب قادرون"
+        />
 
-      <div className="flex flex-wrap justify-between gap-15 m-5 md:justify-center text-center sm:justify-center cards-containerr mt-16">
-        {isLoading || isPageLoading ? (
-          <Loader />
-        ) : (
-          displayedArticles.map(({ _id, title, Img }) => (
-            <Link key={_id} to={`/article/${_id}`}>
-              <Card
-                description={title}
-                image={`${import.meta.env.VITE_HOST_SERVER}imgs/` + Img}
-              />
-            </Link>
-          ))
-        )}
-      </div>
+        {/* Open Graph tags for social sharing */}
+        <meta property="og:title" content="قادرون | آخر المقالات" />
+        <meta
+          property="og:description"
+          content="تصفح أحدث المقالات التعليمية والتنموية في مختلف المجالات على منصة قادرون. استفد من خبراتنا في تطوير الذات وتحسين الجودة الحياتية."
+        />
+        <meta property="og:type" content="website" />
+        <meta
+          property="og:image"
+          content={`${import.meta.env.VITE_HOST_SERVER}imgs/logo.png`}
+        />
+        <meta property="og:url" content={window.location.href} />
 
-      {!isLoading && !isEmptyArray(articles) && (
-        <div className="flex justify-center mt-8 space-x-2">
-          <button
-            onClick={() => handlePageClick(currentPage - 1)}
-            disabled={currentPage === 0}
-            className={`px-4 py-2 bg-gray-300 rounded hover:bg-gray-400 transition-colors ${
-              currentPage === 0 ? "opacity-50 cursor-not-allowed" : ""
-            }`}>
-            السابق
-          </button>
+        {/* Twitter Card tags */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@QaderoonPlatform" />
+        <meta name="twitter:title" content="قادرون | مقالات تنموية وتثقيفية" />
+        <meta
+          name="twitter:description"
+          content="استكشف مكتبتنا الغنية بالمقالات في مجالات التنمية الشخصية والصحة النفسية. تحديثات دورية بمحتوى قيّم ومفيد للجميع."
+        />
+        <meta
+          name="twitter:image"
+          content={`${import.meta.env.VITE_HOST_SERVER}imgs/logo-twitter.png`}
+        />
+      </Helmet>
+      <div className="py-11 pb-16">
+        <h2 className="font-extrabold text-3xl md:text-4xl pb-5 underlineR text-neutral-950 mx-auto">
+          اخر <span className="text-primary relative top-1">المقالات</span>
+        </h2>
 
-          {currentPage > 0 && (
+        <div className="flex flex-wrap justify-between gap-15 m-5 md:justify-center text-center sm:justify-center cards-containerr mt-16">
+          {isLoading || isPageLoading ? (
+            <Loader />
+          ) : (
+            displayedArticles.map(({ _id, title, Img }) => (
+              <Link key={_id} to={`/article/${_id}`}>
+                <Card
+                  description={title}
+                  image={`${import.meta.env.VITE_HOST_SERVER}imgs/` + Img}
+                />
+              </Link>
+            ))
+          )}
+        </div>
+
+        {!isLoading && !isEmptyArray(articles) && (
+          <div className="flex justify-center mt-8 space-x-2">
             <button
               onClick={() => handlePageClick(currentPage - 1)}
-              className="px-4 py-2 bg-gray-300 rounded hover:bg-gray-400 transition-colors">
-              {currentPage}
+              disabled={currentPage === 0}
+              className={`px-4 py-2 bg-gray-300 rounded hover:bg-gray-400 transition-colors ${
+                currentPage === 0 ? "opacity-50 cursor-not-allowed" : ""
+              }`}>
+              السابق
             </button>
-          )}
 
-          <button className="px-4 py-2 bg-primary text-white rounded cursor-default" disabled>
-            {currentPage + 1}
-          </button>
+            {currentPage > 0 && (
+              <button
+                onClick={() => handlePageClick(currentPage - 1)}
+                className="px-4 py-2 bg-gray-300 rounded hover:bg-gray-400 transition-colors">
+                {currentPage}
+              </button>
+            )}
 
-          {currentPage < totalPages - 1 && (
+            <button
+              className="px-4 py-2 bg-primary text-white rounded cursor-default"
+              disabled>
+              {currentPage + 1}
+            </button>
+
+            {currentPage < totalPages - 1 && (
+              <button
+                onClick={() => handlePageClick(currentPage + 1)}
+                className="px-4 py-2 bg-gray-300 rounded hover:bg-gray-400 transition-colors">
+                {currentPage + 2}
+              </button>
+            )}
+
             <button
               onClick={() => handlePageClick(currentPage + 1)}
-              className="px-4 py-2 bg-gray-300 rounded hover:bg-gray-400 transition-colors">
-              {currentPage + 2}
+              disabled={currentPage >= totalPages - 1}
+              className={`px-4 py-2 bg-gray-300 rounded hover:bg-gray-400 transition-colors ${
+                currentPage >= totalPages - 1
+                  ? "opacity-50 cursor-not-allowed"
+                  : ""
+              }`}>
+              التالي
             </button>
-          )}
-
-          <button
-            onClick={() => handlePageClick(currentPage + 1)}
-            disabled={currentPage >= totalPages - 1}
-            className={`px-4 py-2 bg-gray-300 rounded hover:bg-gray-400 transition-colors ${
-              currentPage >= totalPages - 1 ? "opacity-50 cursor-not-allowed" : ""
-            }`}>
-            التالي
-          </button>
-        </div>
-      )}
-    </div>
+          </div>
+        )}
+      </div>
+    </>
   );
 }
 
