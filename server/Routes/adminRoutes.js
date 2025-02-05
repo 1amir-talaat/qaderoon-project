c2385720f25ae92c06668f3e69537e306407f3db
AhmedEweesKorany <ahmedewaisvi34@gmail.com>
2025-02-05 16:31:33 +0200

    refactor(server): remove unused admin registration route
    - Removed unused `registerAdmin` route.
    - Simplified admin routes file.

--- server/Routes/adminRoutes.js
+++ server/Routes/adminRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const AdminController = require('../Controllers/adminController');
 // Add Admin
-router.post('/add', AdminController.registerAdmin);
+// router.post('/add', AdminController.registerAdmin);
     // Admin Login
 router.post('/login', AdminController.loginAdmin);
 // Admin verify
