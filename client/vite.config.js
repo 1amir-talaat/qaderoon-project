a9961c03397f1b1da313f3d3f0b069adea486af8
AhmedEweesKorany <ahmedewaisvi34@gmail.com>
2025-02-06 14:13:54 +0200

    build(client): add vite-plugin-compression
    - Added vite-plugin-compression for
      better performance.
    - Updated package.json and package-lock.json.
    - Configured compression plugin in vite.config.js.

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,10 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import compression from "vite-plugin-compression";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [tailwindcss(), react()],
+  plugins: [tailwindcss(), react(),
+    compression({ algorithm: "brotliCompress",threshold:1024 })
+  ],
   server: {
     // port: 2000,
     // host:"192.168.1.11"
